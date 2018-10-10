58aef2e761253dc9dfb455a7b4289fac2f004b24
jags <jags.santiago@gmail.com>
2018-10-10 17:04:57 -0600

    css plantillas dinamicas y envio de datos de la base

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .models import Post
 
-# Create your views here.
+def listar(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/listar.html', {'posts':posts})
