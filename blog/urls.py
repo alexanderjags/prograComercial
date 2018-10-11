c928cd07096d3dcb12ec5bb941b507fdb5b275c5
jags <jags.santiago@gmail.com>
2018-10-10 19:46:20 -0600

    Se agrega la parte de los formularios de insertar y editar en el blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import include, url
-
+from django.shortcuts import redirect
 from . import views
 
 urlpatterns = [
@@ -7,5 +7,7 @@ urlpatterns = [
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', views.listar),
-    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail,  name='post_detail'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
 ]
